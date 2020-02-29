02e4c56bd8f73b606d060d5c8d3e7eebc64fbdbb
APaulaMiranda <apaulafmiranda@gmail.com>
2020-02-29 22:48:14 +0100

    now the sort works as well yolo

--- src/App.js
+++ src/App.js
@@ -22,51 +22,12 @@ let users = [{
 }];
 
 
-class App extends Component {
-
-  state = {
-    search: "",
-    users: users
-  }
-
-  filterUsers = event => {
-    var value = event.target.value;
-    let filteredUsers = users.filter(user => user.name.toLocaleLowerCase()
-      .includes(value.toLocaleLowerCase()));
-
-    this.setState({
-      search: value,
-      users: filteredUsers
-    });
-  }
-
-  orderUsers = event => {
-    var value = event.target.value;
-    let filteredUsers = users.sort(user => user.team.toLocaleLowerCase()
-      .includes(value.toLocaleLowerCase()));
-
-    this.setState({
-      search: value,
-      users: filteredUsers
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <form className="main form my-3 p-3 bg-white rounded box-shadow container">
-          <input
-            className="form-control"
-            name="search"
-            onChange={this.filterUsers}
-            type="text"
-            placeholder="Search Employee..."
-          />
-        </form>
-        <Employees list={this.state.users} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Employees list={users} />
+    </div>
+  );
 }
 
 export default App;
--- src/Employees.js
+++ src/Employees.js
@@ -1,23 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
 import Employee from "./Employee";
 
-function Employees({ list }) {
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Team</th>
-                    <th>Country</th>
-                </tr>
-            </thead>
-            <tbody>
-                {list.map(element => (
-                    <Employee key={element.id} employee={element} />
-                ))}
-            </tbody>
-        </table>
-    );
+class Employees extends Component {
+
+    state = {
+        employees: this.props.list
+    }
+
+    filterUsers = event => {
+        let value = event.target.value;
+        let filteredUsers = this.props.list.filter(
+            user => user.name.toLocaleLowerCase()
+                .includes(value.toLocaleLowerCase()));
+        this.setState({
+            employees: filteredUsers
+        });
+    }
+
+    orderByCountry = () => {
+        let sortedUsers = this.props.list.sort((user1, user2) => {
+            var keyA = user1.country;
+            var keyB = user2.country;
+            if (keyA < keyB) return -1;
+            if (keyA > keyB) return 1;
+            return 0;
+        });
+        this.setState({ employees: sortedUsers })
+    }
+    render() {
+        return (
+            <div>
+                <form className="main form my-3 p-3 bg-white rounded box-shadow container">
+                    <input
+                        className="form-control"
+                        name="search"
+                        onChange={this.filterUsers}
+                        type="text"
+                        placeholder="Search Employee..."
+                    />
+                </form>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Team</th>
+                            <th onClick={this.orderByCountry}>Country</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.employees.map(element => (
+                            <Employee key={element.id} employee={element} />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+
+    }
 }
 
 export default Employees;
